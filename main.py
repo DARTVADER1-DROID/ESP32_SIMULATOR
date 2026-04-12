76aee5674b93a0c81d306de051cd400c9f3a85fb
Kesavaram <kesavaram.balaji.a@gmail.com>
2026-04-12 19:00:35 +0530

    done

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ app = FastAPI(title="ESP32 Pin Simulator", version="1.0.0", description="Backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
